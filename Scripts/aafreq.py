724fddde13c41e5e5a4c6906b29b5e46833ce0ba
kellyasim <kelly.sim@utexas.edu>
2018-04-22 15:06:54 -0500

    added number of sequences

--- Scripts/aafreq.py
+++ Scripts/aafreq.py
@@ -34,18 +34,20 @@ def main():
   fileoutput = ""
   aminoacidstrand = ""
 
-  txt = open("/Bioinformatics_Project/Data/DatabaseVRegionAA.txt", "r")
-  output = open("/Bioinformatics_Project/Scripts/Output/DatabaseVOut.txt", "w")
+  txt = open("/Bioinformatics_Project/Data/DatabaseCRegionAA.txt", "r")
+  output = open("/Bioinformatics_Project/Scripts/Output/DatabaseCOut.txt", "w")
 
   dictionary = {"A":0, "C":0, "D":0, "E":0, "F":0, "G":0, "H":0, "I":0, "K":0, "L":0, "M":0, "N":0, "P":0, "Q":0, "R":0, "S":0, "T":0, "V":0, "W":0, "Y":0}
   length = 0
   ratio = {}
   ans = ""
+  numberseqences=0
+
 
   for line in txt:
     #piece the aminoacid sequences together
     if line[0] == ">":
-      pass
+      numberseqences+=1
 
     else:
       line = line.strip("\n\t")
@@ -63,7 +65,8 @@ def main():
   for i in sorted(ratio):
     ans = ans + str(ratio[i]) + "\t"
 
-  ans += str(length)
+  ans += str(length) + "\n"
+  ans += str(numberseqences)
 
   print (ans)
   output.write(str(ans))
