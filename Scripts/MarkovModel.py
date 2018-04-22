4b9d3d4d0789e4d20ab8bff0283a40a9983c73ed
Dclove123 <kvaldez2000@gmail.com>
2018-04-22 14:11:17 -0500

    MarkovModel script

---
+++ Scripts/MarkovModel.py
@@ -0,0 +1,12 @@
+def main():
+    frequencyFileC = "/Scripts/Output/DatabaseCOut.txt"
+    frequencyFileV = "/Scripts/Output/DatabaseVOut.txt"
+    fileC = open(frequencyFileC, "r")
+    fileV = open(frequencyFileV, "r")
+    result = open("/Scripts/Output/ViterbiOutput.txt"
+
+
+
+    
+
+main()
