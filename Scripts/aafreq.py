4100727a23170bd88a854eb509e3b5fb06875302
kellyasim <kelly.sim@utexas.edu>
2018-04-22 14:51:30 -0500

    Parsed through both V and C regions

--- Scripts/aafreq.py
+++ Scripts/aafreq.py
@@ -1,7 +1,6 @@
-def aminoacidfrequency(name, sequence, output):
+'''
+def aminoacidfrequency(dictionary, sequence, output):
   #only count the 20 amino acids
-  aminoacid = {"A":0, "C":0, "D":0, "E":0, "F":0, "G":0, "H":0, "I":0, "K":0, "L":0, "M":0, "N":0, "P":0, "Q":0, "R":0, "S":0, "T":0, "V":0, "W":0, "Y":0}
-  length = 0
   index = 0
   ratio = {}
   ans = ""
@@ -10,17 +9,14 @@ def aminoacidfrequency(name, sequence, output):
   while index < len(sequence):
     length += 1
     letter = str(sequence[index])
-    if letter in aminoacid:
-      aminoacid[letter] += 1
+    if letter in dictionary:
+      dictionary[letter] += 1
     index += 1
-  #name = name.strip("\n")
-  #ans = "\n" + name + "\t"
-  #print ()
 
   # calculate ratios of each amino acid
-  for n in aminoacid.keys():
-    freq = aminoacid[n] / length
-    number = aminoacid[n]
+  for n in dictionary.keys():
+    freq = dictionary[n] / length
+    number = dictionary[n]
     ratio[n] = round(freq,5)
 
   # amino acids in alpha order  
@@ -29,7 +25,7 @@ def aminoacidfrequency(name, sequence, output):
 
   print (ans)
   output.write(str(ans))
-
+'''
 
 
 
@@ -41,23 +37,37 @@ def main():
   txt = open("/Bioinformatics_Project/Data/DatabaseVRegionAA.txt", "r")
   output = open("/Bioinformatics_Project/Scripts/Output/DatabaseVOut.txt", "w")
 
-
+  dictionary = {"A":0, "C":0, "D":0, "E":0, "F":0, "G":0, "H":0, "I":0, "K":0, "L":0, "M":0, "N":0, "P":0, "Q":0, "R":0, "S":0, "T":0, "V":0, "W":0, "Y":0}
+  length = 0
+  ratio = {}
+  ans = ""
 
   for line in txt:
     #piece the aminoacid sequences together
     if line[0] == ">":
       pass
 
-    else line[0] != ">":
-    #  newname = line
-      pass
-    fileoutput += aminoacidstrand
-    aminoacidfrequency(previousname, aminoacidstrand, output)
+    else:
+      line = line.strip("\n\t")
+      for char in line:
+        if char in dictionary:
+          dictionary[char] += 1
+        length+=1
 
-      #reset for next protein
-    #  aminoacidstrand = ""
-    #  previousname = newname
-    #input()
+  for n in dictionary.keys():
+    freq = dictionary[n] / length
+    number = dictionary[n]
+    ratio[n] = round(freq,5)
+
+  # amino acids in alpha order  
+  for i in sorted(ratio):
+    ans = ans + str(ratio[i]) + "\t"
+
+  ans += str(length)
+
+  print (ans)
+  output.write(str(ans))
+
   output.close()
   txt.close()
 
