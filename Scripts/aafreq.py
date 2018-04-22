b070181c894351f593527a9ded7b81b88b4975e3
kellyasim <kelly.sim@utexas.edu>
2018-04-22 15:09:07 -0500

    tabs instead of enter

--- Scripts/aafreq.py
+++ Scripts/aafreq.py
@@ -34,8 +34,8 @@ def main():
   fileoutput = ""
   aminoacidstrand = ""
 
-  txt = open("/Bioinformatics_Project/Data/DatabaseCRegionAA.txt", "r")
-  output = open("/Bioinformatics_Project/Scripts/Output/DatabaseCOut.txt", "w")
+  txt = open("/Bioinformatics_Project/Data/DatabaseVRegionAA.txt", "r")
+  output = open("/Bioinformatics_Project/Scripts/Output/DatabaseVOut.txt", "w")
 
   dictionary = {"A":0, "C":0, "D":0, "E":0, "F":0, "G":0, "H":0, "I":0, "K":0, "L":0, "M":0, "N":0, "P":0, "Q":0, "R":0, "S":0, "T":0, "V":0, "W":0, "Y":0}
   length = 0
@@ -65,7 +65,7 @@ def main():
   for i in sorted(ratio):
     ans = ans + str(ratio[i]) + "\t"
 
-  ans += str(length) + "\n"
+  ans += str(length) + "\t"
   ans += str(numberseqences)
 
   print (ans)
