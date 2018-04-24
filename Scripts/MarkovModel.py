7b26f413f1ba5d8be22cc6d5fe953c8e244786b7
Dclove123 <kvaldez2000@gmail.com>
2018-04-24 13:45:41 -0500

    changes to script and re-run output

--- Scripts/MarkovModel.py
+++ Scripts/MarkovModel.py
@@ -5,13 +5,13 @@ def main():
     inputL = open(testingDataL,"r")
     fileC = open(frequencyFileC, "r")
     fileV = open(frequencyFileV, "r")
-    result = open("/Bioinformatics_Project/Scripts/Output/ViterbiOutput.txt", "w")
+    result = open("/Bioinformatics_Project/Scripts/Output/FixedViterbiOutput.txt", "w")
 
     resultTuple = ("",10.0e256)
 
     
-    beta = {"C": 1, "V": 0}
-    stateChangeFreq = {"CC": 0.99057, "CV": .00943, "VC": 0, "VV": 1}
+    ##stateChangeFreq = {"CC": 0.99057, "CV": .00943, "VC": .010638, "VV": 0.98936}
+    stateChangeFreq = {"CC": 1, "CV": 0, "VC": .010638, "VV": 0.98936}
     
     aminoAcidDict = {"A":0,"C":0,"D":0,"E":0,"F":0,"G":0,"H":0,"I":0,"K":0,"L":0,"M":0,"N":0,"P":0,"Q":0,"R":0,"S":0,"T":0,"V":0,"W":0,"Y":0}
     print(aminoAcidDict)
@@ -23,24 +23,80 @@ def main():
     
     for line in inputL:
         if line[0] == ">" or line[0] == "\n":
-            #print(line)
+            print(line)
             result.write(line)
         else:
-            #print(line)
+            print(line)
             result.write(line)
             line = line.rstrip("\n")
             line = line.replace("*","")
             resultTuple = Viterbi(line,aaFreqC,aaFreqV,stateChangeFreq,resultTuple,True)
-            #print(resultTuple[0])
-            result.write(resultTuple[0])
+            print(resultTuple[0] , " " , resultTuple[1])
+            result.write(resultTuple[0] + "\n")
             resultTuple = ("",10.0e256)
     
     test = fileC.readline()
     print(test)
 
-def Viterbi(sequence,dictC,dictV,dictState,result,isC):     #Returns highest valued frequency
+def Viterbi(sequence,dictC,dictV,dictState,result,isV):     #Returns highest valued frequency
     #print(sequence)
     if result[1] == 0:
+        #print("FLOAT OVERFLOW")
+        return result
+    if sequence == "":
+        result = list(result)
+        if isV:
+            result[0] = result[0] + "V"   
+        else:
+            result[0] = result[0] + "C"
+        result = tuple(result)
+        return result
+    if result[0] == "":     #Make a beginning state here
+        tempOne = (result[0],result[1])
+        tempTwo = (result[0],result[1])
+    elif result[0][-1] == "V":
+        tempOne = (result[0],ViterbiCalc(sequence,dictV,dictState["VV"],result[1]))
+        tempTwo = (result[0],ViterbiCalc(sequence,dictC,dictState["VC"],result[1]))
+    elif result[0][-1] == "C":
+        #print("V'd")
+        tempOne = (result[0],ViterbiCalc(sequence,dictC,dictState["CC"],result[1]))
+        tempOne = list(tempOne)
+        tempOne[0] = tempOne[0] + "C"
+        tempOne = tuple(tempOne)
+        finalResultC = Viterbi(sequence[1:],dictC,dictV,dictState,tempOne,False)
+        #print(finalResultC)
+        return finalResultC
+    if isV:
+        tempOne = list(tempOne)
+        tempOne[0] = tempOne[0] + "V"
+        tempOne = tuple(tempOne)
+        if result[0] == "":
+            tempTwo = list(tempTwo)
+            tempTwo[0] = tempTwo[0] + "V"
+            tempTwo = tuple(tempTwo)
+        else:
+            tempTwo = list(tempTwo)
+            tempTwo[0] = tempTwo[0] + "C"
+            tempTwo = tuple(tempTwo)
+
+    
+    finalResultV = Viterbi(sequence[1:],dictC,dictV,dictState,tempOne,True)
+    finalResultC = Viterbi(sequence[1:],dictC,dictV,dictState,tempTwo,False)
+
+
+
+    if finalResultV[1] > finalResultC[1]:
+        #print(finalResultC)
+        return finalResultV
+    else:
+        #print(finalResultV)
+        return finalResultC
+
+    return result
+
+
+
+    """if result[1] == 0:
         return result
     if sequence == "":
         return result
@@ -86,7 +142,7 @@ def Viterbi(sequence,dictC,dictV,dictState,result,isC):     #Returns highest val
         #print(finalResultV)
         return finalResultV
 
-    return result
+    return result"""
         
 def ViterbiCalc(sequence,dict,stateMult,probability):
     temp = probability * float(dict[sequence[0]]) * float(stateMult)
