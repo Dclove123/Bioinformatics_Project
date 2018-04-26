1492c155aa08745fddd6e5847189b21fbd552206
Dclove123 <kvaldez2000@gmail.com>
2018-04-25 19:52:24 -0500

    Full update of HMM and Viterbi to function for LV

--- Scripts/MarkovModel.py
+++ Scripts/MarkovModel.py
@@ -1,27 +1,32 @@
 def main():
     frequencyFileC = "/Bioinformatics_Project/Scripts/Output/DatabaseCOut.txt"
     frequencyFileV = "/Bioinformatics_Project/Scripts/Output/DatabaseVOut.txt"
-    testingDataL = "/Bioinformatics_Project/Scripts/Output/LSeqOut.txt"
-    inputL = open(testingDataL,"r")
+    frequencyFileL = "/Bioinformatics_Project/Scripts/Output/DatabaseLOut.txt"
+    
+    testingDataLV = "/Bioinformatics_Project/Scripts/Output/LVSeqOut.txt"
+    inputLV = open(testingDataLV,"r")
     fileC = open(frequencyFileC, "r")
     fileV = open(frequencyFileV, "r")
-    result = open("/Bioinformatics_Project/Scripts/Output/FixedViterbiOutput.txt", "w")
+    fileL = open(frequencyFileL, "r")
+    result = open("/Bioinformatics_Project/Scripts/Output/LVFixedViterbiOutput.txt", "w")
 
     resultTuple = ("",10.0e256)
 
     
     ##stateChangeFreq = {"CC": 0.99057, "CV": .00943, "VC": .010638, "VV": 0.98936}
-    stateChangeFreq = {"CC": 1, "CV": 0, "VC": .010638, "VV": 0.98936}
+    stateChangeFreq = {"LL": 0.9333333, "LV": 0.06666667, "VL": 0, "VV": 1}
     
     aminoAcidDict = {"A":0,"C":0,"D":0,"E":0,"F":0,"G":0,"H":0,"I":0,"K":0,"L":0,"M":0,"N":0,"P":0,"Q":0,"R":0,"S":0,"T":0,"V":0,"W":0,"Y":0}
     print(aminoAcidDict)
     
     aaFreqC = fillDict(aminoAcidDict.copy(),fileC.readline())
     print(aaFreqC)
+    aaFreqL =fillDict(aminoAcidDict.copy(),fileL.readline())
+    print(aaFreqL)
     aaFreqV = fillDict(aminoAcidDict.copy(),fileV.readline())
     print(aaFreqV)
     
-    for line in inputL:
+    for line in inputLV:
         if line[0] == ">" or line[0] == "\n":
             print(line)
             result.write(line)
@@ -30,7 +35,7 @@ def main():
             result.write(line)
             line = line.rstrip("\n")
             line = line.replace("*","")
-            resultTuple = Viterbi(line,aaFreqC,aaFreqV,stateChangeFreq,resultTuple,True)
+            resultTuple = Viterbi(line,aaFreqL,aaFreqV,stateChangeFreq,resultTuple,True)
             print(resultTuple[0] , " " , resultTuple[1])
             result.write(resultTuple[0] + "\n")
             resultTuple = ("",10.0e256)
@@ -38,11 +43,11 @@ def main():
     test = fileC.readline()
     print(test)
 
-def Viterbi(sequence,dictC,dictV,dictState,result,isV):     #Returns highest valued frequency
+def Viterbii(sequence,dictC,dictV,dictState,result,isV):     #Returns highest valued frequency
     #print(sequence)
-    if result[1] == 0:
+    """if result[1] == 0:
         #print("FLOAT OVERFLOW")
-        return result
+        return result"""
     if sequence == "":
         result = list(result)
         if isV:
@@ -143,11 +148,71 @@ def Viterbi(sequence,dictC,dictV,dictState,result,isV):     #Returns highest val
         return finalResultV
 
     return result"""
+
+def Viterbi(sequence,dictL,dictV,dictState,result,isL):
+    if result[1] == 0:
+        return result
+    if sequence == "":
+        result = list(result)
+        if isL:
+            result[0] = result[0] + "L"   
+        else:
+            result[0] = result[0] + "V"
+        result = tuple(result)
+        return result
+    if result[0] == "":     #Make a beginning state here
+        tempOne = (result[0],result[1])
+        tempTwo = (result[0],result[1])
+    elif result[0][-1] == "L":
+        tempOne = (result[0],ViterbiCalc(sequence,dictL,dictState["LL"],result[1]))
+        tempTwo = (result[0],ViterbiCalc(sequence,dictV,dictState["LV"],result[1]))
+    elif result[0][-1] == "V":
+        #print("V'd")
+        tempOne = (result[0],ViterbiCalc(sequence,dictV,dictState["VV"],result[1]))
+        tempOne = list(tempOne)
+        tempOne[0] = tempOne[0] + "V"
+        tempOne = tuple(tempOne)
+        finalResultV = Viterbi(sequence[1:],dictL,dictV,dictState,tempOne,False)
+        #print(finalResultV)
+        return finalResultV
+        #tempTwo = (result[0],ViterbiCalc(sequence,dictL,dictState["VC"],result[1]))
+    if isL:
+        tempOne = list(tempOne)
+        tempOne[0] = tempOne[0] + "L"
+        tempOne = tuple(tempOne)
+        if result[0] == "":
+            tempTwo = list(tempTwo)
+            tempTwo[0] = tempTwo[0] + "L"
+            tempTwo = tuple(tempTwo)
+        else:
+            tempTwo = list(tempTwo)
+            tempTwo[0] = tempTwo[0] + "V"
+            tempTwo = tuple(tempTwo)
+
+    
+    finalResultL = Viterbi(sequence[1:],dictL,dictV,dictState,tempOne,True)
+    finalResultV = Viterbi(sequence[1:],dictL,dictV,dictState,tempTwo,False)
+
+
+
+    if finalResultL[1] > finalResultV[1]:
+        #print(finalResultL)
+        return finalResultL
+    else:
+        #print(finalResultV)
+        return finalResultV
+
+    return result
         
 def ViterbiCalc(sequence,dict,stateMult,probability):
-    temp = probability * float(dict[sequence[0]]) * float(stateMult)
+    try:
+        temp = probability * float(dict[sequence[0]]) * float(stateMult)
+        return temp
+    except:
+        return probability
+    """temp = probability * float(dict[sequence[0]]) * float(stateMult)
     #print(temp)
-    return temp
+    return temp"""
 
 def fillDict(dictionary, data):
     temp = data.split("\t")
